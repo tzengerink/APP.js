8274ac6f47a2a72dd0e495d092936b3fc7050c38
T. Zengerink <t.zengerink@gmail.com>
2012-10-01 11:56:42 +0200

    minor code refactoring

---
+++ src/APP/Core.js
@@ -0,0 +1,35 @@
+/*
+ * Core
+ * ----
+ *
+ * Core utilities for the application.
+ *
+ * Copyright (c) 2012, T. Zengerink
+ * Licensed under MIT License.
+ * See: https://raw.github.com/Mytho/APP.js/master/LISENCE.md
+ */
+createModule("APP.Core", function(){
+
+	var Core = {},
+		logHistory = [];
+
+	// PUBLIC
+	// ------
+
+	Core.log = function(){
+		logHistory.push(arguments);
+		if (win.console) {
+			console.log(Array.prototype.slice.call(arguments));
+		}
+	};
+
+	Core.init = function(){
+		// TODO: finish auto initializing of submodules.
+	};
+
+	// SETUP
+	// -----
+
+	return Core;
+
+})();
