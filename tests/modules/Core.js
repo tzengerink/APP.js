47c646cb3b100f8befdd13bd29e3803b350e1ad8
T. Zengerink <t.zengerink@gmail.com>
2012-11-28 14:10:56 +0100

    modified and added tests

--- tests/modules/Core.js
+++ tests/modules/Core.js
@@ -1,10 +1,13 @@
 module("APP.Core");
 
-// module
-// ------
+test("ready", function(){
+	var testvar1 = false;
+	APP.ready(function(){ testVar1 = true; });
+	expect(1);
+	ok(testVar1, "DOM Ready.");
+});
 
 test("module", function(){
-
 	APP.module("APP.TestModule", [window, document], function(win, doc){
 		return {
 			"docObj" : doc,
@@ -14,7 +17,6 @@ test("module", function(){
 			}
 		};
 	});
-
 	APP.module("APP.TestModule.SubModule", function(){
 		return {
 			"start" : function(){
@@ -22,84 +24,53 @@ test("module", function(){
 			}
 		};
 	});
-
 	APP.module("APP.TestModule.testMethod", function(){
 		return function(str){
 			return str == "test";
 		};
 	});
-
 	APP.module("APP.TestModule.testVar", 1234);
 	APP.module("APP.TestModule.testObject", { "key" : "value" });
-
 	expect(5);
-
 	equal(APP.TestModule.start(), true, "Define module.");
 	equal(APP.TestModule.SubModule.start(), true, "Define submodule.");
 	equal(APP.TestModule.testMethod("test"), true, "Define method.");
 	equal(APP.TestModule.testVar, 1234, "Define variable.");
 	deepEqual(APP.TestModule.testObject, { "key" : "value" }, "Define object.");
-
 });
 
-// config
-// ------
-
 test("config", function(){
-
 	expect(4);
-
 	equal(typeof APP.Core.config(), "object", "Get configuration object.");
 	equal(typeof APP.Core.config("nonExistingKey"), "undefined", "Get undefined config key.");
 	equal(typeof APP.Core.config({ "key" : "value" }), "object", "Set config object.");
 	equal(APP.Core.config("key"), "value", "Get config variable.");
-
 });
-
-// Log
-// ---
 
 test("Log", function(){
-
 	log("test1");
 	log("test2");
-
 	expect(1);
-
 	deepEqual(APP.Core.Log.history, ["test1", "test2"], "Append log history.");
-
 });
 
-// Url
-// ---
-
 test("Url", function(){
-
 	var testUriOne = "some/long/uri",
 		testUriTwo = "/some/long/uri/",
 		protocol = window.location.protocol,
 		host = window.location.host,
 		base = protocol + "//" + host + "/" + testUriOne;
-
 	expect(3);
-
 	APP.Core.config({ baseUri:testUriOne });
 	equal(APP.Core.Url.base(), base, "Get base URL.");
-
 	APP.Core.config({ baseUri:testUriTwo });
 	equal(APP.Core.Url.site("/test/"), base + "/test", "Strip slashes from provided URI.");
 	equal(APP.Core.Url.site("test"), base + "/test", "Strip slashes from provided URI.");
-
 });
 
-// start
-// -----
-
 test("start", function(){
-
 	var moduleVar = false,
 		subModuleVar = false;
-
 	APP.module("APP.Mod", function(){
 		return {
 			start : function(){
@@ -107,7 +78,6 @@ test("start", function(){
 			}
 		}
 	});
-
 	APP.module("APP.Mod.Sub", function(){
 		return {
 			start : function(){
@@ -115,25 +85,16 @@ test("start", function(){
 			}
 		}
 	});
-
 	APP.start({ "key" : "value" });
-
 	expect(3);
-
 	ok(moduleVar, "Start modules.");
 	ok(subModuleVar, "Start submodules.");
 	equal(APP.Core.config("key"), "value", "Set configuration.");
-
 });
 
-// stop
-// ----
-
 test("stop", function(){
-
 	var moduleVar = false,
 		subModuleVar = false;
-
 	APP.module("APP.Mod", function(){
 		return {
 			stop : function(){
@@ -141,7 +102,6 @@ test("stop", function(){
 			}
 		}
 	});
-
 	APP.module("APP.Mod.Sub", function(){
 		return {
 			stop : function(){
@@ -149,12 +109,8 @@ test("stop", function(){
 			}
 		}
 	});
-
 	APP.stop();
-
 	expect(2);
-
 	ok(moduleVar, "Stop modules.");
 	ok(subModuleVar, "Stop submodules.");
-
 });
--- tests/modules/PubSub.js
+++ tests/modules/PubSub.js
@@ -1,48 +1,25 @@
 module("APP.PubSub");
 
-// on
-// --
-
 test("on", function(){
-
 	var testVar = false;
-
 	APP.PubSub.on("news", function(){ testVar = true; });
 	APP.PubSub.trigger("news");
-
 	expect(1);
-
 	ok(testVar, "Subscribe to channel.");
-
 });
-
-// trigger
-// -------
 
 test("trigger", function(){
-
 	var testVar = false;
-
 	APP.PubSub.on("news", function(str){ testVar = str; });
 	APP.PubSub.trigger("news", "value");
-
 	equal(testVar, "value", "Trigger channel.");
-
 });
 
-// off
-// ---
-
 test("off", function(){
-
 	var testVar = false;
-
 	APP.PubSub.on("news", function(){ testVar = true; });
 	APP.PubSub.off("news");
 	APP.PubSub.trigger("news");
-
 	expect(1);
-
 	equal(testVar, false, "Unsubscribe from channel.");
-
 });
